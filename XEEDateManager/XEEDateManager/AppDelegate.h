343d702a4b969be6d0e40553aa008517941ec1ff
Andrija Cajic <ancajic@gmail.com>
2013-12-09 12:15:01 +0100

    Project setup

---
+++ XEEDateManager/XEEDateManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XEEDateManager
+//
+//  Created by Andrija Cajic on 09/12/13.
+//  Copyright (c) 2013 XEE Tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
